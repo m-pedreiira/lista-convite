8bfd3e8f0b1bb73583df9be573b5e0e50077093f
m-pedreiira <devmateusp@gmail.com>
2024-10-30 21:20:17 -0300

    testando commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <div>oi mateus</div>
         {children}
       </body>
     </html>
